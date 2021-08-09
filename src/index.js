6931212f770491eb20b4b5ce0a91f3b61ac238a6
Nikolay Yaryhin <nik.oroferov@gmail.com>
2021-08-09 09:07:51 +0300

    Create repository

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import './sass/main.scss';
+ class NewsApiService{
+    constructor() {
+        
+        this.KEY = "8b01b570d2ebdf9fc3f47ddefbdd70fc";
+        this.URL = "https://api.themoviedb.org/3/";
+        this.page = 1;
+        this.type = "search";
+        this.media_type = "movie";
+        this.query = "Небо";
+        this.language = "ru-RU";
+        this.adult = false;
+    }
+    
+ async getMoviesRequest() {
+     const URL = `${this.URL}${this.type}/${this.media_type}?api_key=${this.KEY}&query=${this.query}&language=${this.language}&page=${this.page}&include_adult=${this.adult}`;
+     const response = await axios.get(URL);
+     console.log(response.data);
+     return response.data;
+    };
+
+    resetPage() {
+        this.page = 1;
+    };
+}
