a6aab0b77437261aa920fa7d19e56749293a6106
kovalchuktaisiia <kovalchuk.taisiia@google.com>
2021-08-11 21:46:12 +0300

    text commit

---
+++ src/index-2.js
@@ -0,0 +1,27 @@
+const libraryBtn = document.querySelector ('.library__btn');
+const header = document.querySelector('header');
+const libraryFilter = document.querySelector ('.filter');
+const searshForm = document.querySelector ('.search__form');
+const logo = document.querySelector ('.logo__icon');
+const home = document.querySelector ('.home__btn');
+
+
+libraryBtn.addEventListener ('click', openBtns);
+logo.addEventListener ('click', hideBtns);
+home.addEventListener ('click', hideBtns);
+
+function openBtns (evt) {
+    header.style.backgroundImage = 'url(../images/library_desc@2x.jpg)';    
+    libraryFilter.style.visibility = 'visible';
+    libraryFilter.style.display = "block";
+    searshForm.style.visibility = "hidden";
+    searshForm.style.display = "none";
+}
+
+function hideBtns (e){
+    libraryFilter.style.visibility = 'hidden';
+    libraryFilter.style.display = "none";
+    searshForm.style.visibility = "visible";
+    searshForm.style.display = "flex";
+}
+
