8f1eaf826d4fce9e0017a2dd611583be09c5f539
kovalchuktaisiia <kovalchuk.taisiia@google.com>
2021-08-11 23:17:30 +0300

    templates

--- src/index-2.js
+++ src/index-2.js
@@ -11,6 +11,7 @@ logo.addEventListener ('click', hideBtns);
 home.addEventListener ('click', hideBtns);
 
 function openBtns (evt) {
+    
     header.style.backgroundImage = 'url(../images/library_desc@2x.jpg)';    
     libraryFilter.style.visibility = 'visible';
     libraryFilter.style.display = "block";
@@ -19,6 +20,7 @@ function openBtns (evt) {
 }
 
 function hideBtns (e){
+    header.style.backgroundImage = 'url(../images/header_desc@2x.jpg)'; 
     libraryFilter.style.visibility = 'hidden';
     libraryFilter.style.display = "none";
     searshForm.style.visibility = "visible";
