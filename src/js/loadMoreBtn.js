aed4f1c90fd6ade6b89cfb5afddbdb698fa623e7
Chantsova Anastasia <stasyadiv@gmail.com>
2021-08-17 19:31:10 +0300

    Сombined pagination
    Add combined pagination(numbers pages, infinite scroll, loadmore btn)

---
+++ src/js/loadMoreBtn.js
@@ -0,0 +1,22 @@
+export default class LoadMoreBtn {
+  constructor({ selector }) {
+    this.refs = this.getRefs(selector);
+  }
+
+  getRefs(selector) {
+    const refs = {};
+    refs.button = document.querySelector(selector);
+    refs.label = refs.button.querySelector('.label');
+
+    return refs;
+  }
+
+  show() {
+    this.refs.button.classList.remove('is-hidden');
+  }
+
+  hide() {
+    this.refs.button.classList.add('is-hidden');
+  }
+  
+  }
