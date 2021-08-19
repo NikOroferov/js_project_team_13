d790eef78c17660ebbe403f2e49220a52360e32f
Nataliia Bokhonska <119nelyaterry@gmail.com>
2021-08-19 19:17:31 +0300

    css

--- src/js/modalByStudents.js
+++ src/js/modalByStudents.js
@@ -10,19 +10,20 @@ function toggleModal(e) {
   refs.closeModalBtn.addEventListener('click', toggleModalClose);
   document.body.style.overflowY = 'hidden';
   refs.modal.classList.toggle('is-hidden');
-    refs.parallaxBody.addEventListener('click', showStudent);    
+  
+  setTimeout(showStudent, 3000);
 }
 
 
 function showStudent() {
-        refs.studentCard.removeEventListener('click', showStudent);
-        refs.parallax.classList.add('is-hidden');
+    refs.studentCard.removeEventListener('click', showStudent);
+    refs.parallax.classList.add('is-hidden');
     refs.studentCard.classList.add('is-active');
     let number = 0;
 
     onCreateCard();
 
-        function onCreateCard() {
+    function onCreateCard() {
             
             
       if (refs.modal.classList.contains('is-hidden')) {
@@ -37,40 +38,40 @@ function showStudent() {
             const endMarkup = `<p class='thanks'>Спасибо за внимание!</p>
             <table class="desc__table">
     
-  <tr class="decs__item">
-    <td class="desc">Cкрам-мастер, помощник режиссера и художника по спецэффектам</td>
-    <td class="desc__name">Nataliia</td>
-    <td class="desc__link">https://github.com/nelyaterry</td>
-    <tr class="decs__item">
-    <td class="desc">Помощник режиссера, властелин LocalStorage</td>
-    <td class="desc__name">Katerina</td>
-    <td class="desc__link">https://github.com/Kateryna286</td>
-    <tr class="decs__item">
-    <td class="desc">Художник по спецэффектам, техник по пагинации и параллаксу</td>
-    <td class="desc__name">Anastasia</td>
-    <td class="desc__link">https://github.com/Chantsova</td>
-    <tr class="decs__item">
-    <td class="desc">Дизайнер, покоритель get-запросов</td>
-    <td class="desc__name">Maks</td>
-    <td class="desc__link">https://github.com/Maksym1994</td>
-    <tr class="decs__item">
-    <td class="desc">Художник по свету,  консультант по переходу на тёмную сторону</td>
-    <td class="desc__name">Vasilisa</td>
-    <td class="desc__link">https://github.com/Vasiliska-A</td>
-    <tr class="decs__item">
-    <td class="desc">Декоратор и оформитель модальных окон</td>
-    <td class="desc__name">Aleksei</td>
-    <td class="desc__link">https://github.com/alexey-chernov</td>
-    <tr class="decs__item">
-    <td class="desc">Художник-оформитель, дизайнер</td>
-    <td class="desc__name">Taisiia</td>
-    <td class="desc__link">https://github.com/kovalchuktaisiia</td>
-  <tr class="decs__item">
-    <td class="desc__lead">Главный режиссер, тим-лид команды</td>
-    <td class="desc__name">Nikolai</td>
-    <td class="desc__link">'https://github.com/NikOroferov'</td>
-  </p>
-</table>`;
+              <tr class="decs__item">
+                <td class="desc">Cкрам-мастер, помощник режиссера и художника по спецэффектам</td>
+                <td class="desc__name">Nataliia</td>
+                <td class="desc__link">https://github.com/nelyaterry</td>
+                <tr class="decs__item">
+                <td class="desc">Помощник режиссера, властелин LocalStorage</td>
+                <td class="desc__name">Katerina</td>
+                <td class="desc__link">https://github.com/Kateryna286</td>
+                <tr class="decs__item">
+                <td class="desc">Художник по спецэффектам, техник по пагинации и параллаксу</td>
+                <td class="desc__name">Anastasia</td>
+                <td class="desc__link">https://github.com/Chantsova</td>
+                <tr class="decs__item">
+                <td class="desc">Дизайнер, покоритель get-запросов</td>
+                <td class="desc__name">Maks</td>
+                <td class="desc__link">https://github.com/Maksym1994</td>
+                <tr class="decs__item">
+                <td class="desc">Художник по свету,  консультант по переходу на тёмную сторону</td>
+                <td class="desc__name">Vasilisa</td>
+                <td class="desc__link">https://github.com/Vasiliska-A</td>
+                <tr class="decs__item">
+                <td class="desc">Декоратор и оформитель модальных окон</td>
+                <td class="desc__name">Aleksei</td>
+                <td class="desc__link">https://github.com/alexey-chernov</td>
+                <tr class="decs__item">
+                <td class="desc">Художник-оформитель, дизайнер</td>
+                <td class="desc__name">Taisiia</td>
+                <td class="desc__link">https://github.com/kovalchuktaisiia</td>
+              <tr class="decs__item">
+                <td class="desc__lead">Главный режиссер, тим-лид команды</td>
+                <td class="desc__name">Nikolai</td>
+                <td class="desc__link">'https://github.com/NikOroferov'</td>
+              </p>
+            </table>`;
             
             
 
@@ -84,7 +85,7 @@ function showStudent() {
         return;
       }
 
-      let timerId = setTimeout(onCreateCard, 1500);
+      let timerId = setTimeout(onCreateCard, 2000);
     }
 }
   
@@ -94,5 +95,5 @@ function toggleModalClose(e) {
     refs.modal.classList.toggle('is-hidden');
     refs.studentCard.classList.remove('is-active');
     refs.studentCard.removeEventListener('click', showStudent);
-  refs.closeModalBtn.removeEventListener('click', toggleModalClose);
+    refs.closeModalBtn.removeEventListener('click', toggleModalClose);
 }
