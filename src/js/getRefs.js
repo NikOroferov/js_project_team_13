2344abfc28493e8ac4959aadcdde18a5218ba1dd
Nataliia Bokhonska <119nelyaterry@gmail.com>
2021-08-19 07:00:37 +0300

    addstudent

--- src/js/getRefs.js
+++ src/js/getRefs.js
@@ -35,4 +35,6 @@ export const refs = {
   openModalBtn: document.querySelector('[data-modal-open]'),
   closeModalBtn: document.querySelector('[data-modal-close]'),
   modal: document.querySelector('[data-modal]'),
+
+  studentCard: document.querySelector('.students'),
 };
