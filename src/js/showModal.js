fb075300e04cf22f0d195e0f7c102a5b8f5319cd
Nikolay Yaryhin <nik.oroferov@gmail.com>
2021-08-19 14:12:57 +0300

    fix style, modal

--- src/js/showModal.js
+++ src/js/showModal.js
@@ -142,7 +142,7 @@ function getFullMovieInfo(id) {
       closeBtn.addEventListener('click', closeModal);
       window.addEventListener('keydown', closeModalHandler);
       window.addEventListener('keydown', closeModalHandler);
-      // window.addEventListener('click', closeModalHandlerClick);
+      window.addEventListener('click', closeModalHandlerClick);
      
 
       //функція закриття форми по клавіші Esc
@@ -154,21 +154,16 @@ function getFullMovieInfo(id) {
         }
       }
 
-      // function closeModalHandlerClick(evt) {
-      //   console.log(evt.);
-      //   modal.close();
-        
-      //   window.removeEventListener('keydown', closeModalHandlerClick);
-      //   document.body.style.overflowY = "visible";
-
-      // }
+      function closeModalHandlerClick(evt) {
+        document.body.style.overflowY = "visible";
+        window.removeEventListener('click', closeModalHandlerClick);
+      }
 
       function closeModal() {
         modal.close();
         document.body.style.overflowY = "visible";
 
         window.removeEventListener('keydown', closeModalHandler);
-        // window.removeEventListener('keydown', closeModalHandlerClick);
       }
     })
     .catch(error => console.log('error', error));
